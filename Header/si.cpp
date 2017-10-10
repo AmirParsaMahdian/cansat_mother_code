89eb6ea69c62db2327a927fe10a7d8b121714e8b
amir parsa Mahdian <amirparsamahdian@gmail.com>
2017-10-10 20:21:24 +0000

    Replace si.cpp

--- Header/si.cpp
+++ Header/si.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdlib.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
@@ -8,46 +9,35 @@
 #include <time.h>
 #include <iostream>
 #include <stdio.h>
-#include "si.h"
-int readTemp(int file)
-{
-	float temperature;
-	double temp_code;
-	ioctl(file,I2C_SLAVE,0x40);
-    
-	char config[1]={0xE3};
-	write(file,config,1);
-	
-	char data[2];
-	read(file,data,2);
-  	
-    temp_code = (data[0]<<8) + data[1];
-   	temperature = ((175.72 * temp_code)/65536)-46.85;
-	 
-	return (temperature);
+#include <fstream>
+#include "i2c.h"
 
+
+//############## SI7021 ##############
+
+// Reads temperature ( mode = 't' ) and humidity ( mode = 'h' )
+float si7021Read(int bus, char mode)
+{
+	if ( mode == 't')
+	{
+		double temp_code;
+		char *data;
+		data = new char[2];
+		registerRead(bus, 0x40, 0xE3, 2, data);
+		temp_code = (int16_t)(data[0] << 8 | data[1]);
+		
+		return (float)(((175.72 * temp_code)/65536)-46.85);
+	}
+	if ( mode == 'h')
+	{
+		char *data;
+		data = new char[2];
+		registerRead(bus, 0x40, 0xE5, 2, data);
+		double Y_out1, Y_out2;
+		Y_out1 = (125*(data[0]/100)*25600)>>16;
+    	Y_out2 = (125*((data[0]%100)*256+data[1]))>>16;
+    	
+		return (float)(Y_out1 + Y_out2 - 6);
+	}
 }
 
-
-int readRH(int file)
-{
-	float RH;
-	double Y_out1, Y_out2;
-	ioctl(file,I2C_SLAVE,0x40);
-    
-	char config[1]={0xE5};
-	write(file,config,1);
-	
-	char data[2];
-	read(file,data,2);
-	
-	Y_out1 = (125*(data[0]/100)*25600)>>16;
-    Y_out2 = (125*((data[0]%100)*256+data[1]))>>16;
-	
-   	RH = Y_out1 + Y_out2 - 6;
-	 
-	return (RH);
-}
-
-
-//#######  #######
